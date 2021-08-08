335da8844276188196eb49eaafba98bd27aa3323
Yusuf-Goren <yusuff14@hotmail.com>
2021-08-08 20:53:11 +0300

    Updates

---
+++ precache-manifest.ab7d90dd82aa688ae2a1d26ec9e058ed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f1071f3d16f1aaad59bbf135bba385",
+    "url": "/yusufgoren.com/index.html"
+  },
+  {
+    "revision": "f5fd8a4c774c925f6b8a",
+    "url": "/yusufgoren.com/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "14379acaf966be83745f",
+    "url": "/yusufgoren.com/static/css/main.440b196c.chunk.css"
+  },
+  {
+    "revision": "f5fd8a4c774c925f6b8a",
+    "url": "/yusufgoren.com/static/js/2.128578e0.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/yusufgoren.com/static/js/2.128578e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14379acaf966be83745f",
+    "url": "/yusufgoren.com/static/js/main.0345cd9d.chunk.js"
+  },
+  {
+    "revision": "9f0a14a78e50bdf92995",
+    "url": "/yusufgoren.com/static/js/runtime-main.fb124ed8.js"
+  },
+  {
+    "revision": "2d9aab10b930a15a17b91a1064b64624",
+    "url": "/yusufgoren.com/static/media/brdb.2d9aab10.png"
+  },
+  {
+    "revision": "dcc22da7bd19c53cc59741a3179d82f3",
+    "url": "/yusufgoren.com/static/media/flower.dcc22da7.png"
+  },
+  {
+    "revision": "2d9aab10b930a15a17b91a1064b64624",
+    "url": "/yusufgoren.com/static/media/fp.2d9aab10.jpg"
+  },
+  {
+    "revision": "199ad3c93cf438a31bc673b94eb85c7b",
+    "url": "/yusufgoren.com/static/media/kaggle.199ad3c9.png"
+  },
+  {
+    "revision": "5cf738287a58ed1c2b929f95adea5096",
+    "url": "/yusufgoren.com/static/media/logo.5cf73828.png"
+  },
+  {
+    "revision": "e327eef30275e5a917610d4fe288a596",
+    "url": "/yusufgoren.com/static/media/mainpage.e327eef3.png"
+  },
+  {
+    "revision": "2d9aab10b930a15a17b91a1064b64624",
+    "url": "/yusufgoren.com/static/media/screenshot_1.2d9aab10.png"
+  },
+  {
+    "revision": "cb4fb77979097e9abe5997926c8d1230",
+    "url": "/yusufgoren.com/static/media/starwars.cb4fb779.jpg"
+  }
+]);
